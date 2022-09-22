cdcd8033308a58fad302ec5a6ed4c1cdd441f2c3
binhpt310 <theshield310@gmail.com>
2022-09-22 14:06:04 +0700

    update all

--- app/src/main/java/com/example/myapplication/Connectivity.java
+++ app/src/main/java/com/example/myapplication/Connectivity.java
@@ -1,11 +1,23 @@
 package com.example.myapplication;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.nfc.Tag;
 import android.telephony.TelephonyManager;
 import android.text.format.Formatter;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class Connectivity {
 
@@ -63,4 +75,109 @@ public class Connectivity {
         String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
         return ip;
     }
+
+    public static String getWifiSSID (Context context){
+        WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        String ssid = "";
+        WifiInfo wifiInfo = wm.getConnectionInfo();
+        if (wifiInfo.getSupplicantState() == SupplicantState.COMPLETED) {
+            ssid = wifiInfo.getSSID();
+        }
+        return ssid;
+    }
+
+    public static String getSimState(Context context){
+        TelephonyManager telephony_manager = (TelephonyManager)
+                context.getSystemService(Context.TELEPHONY_SERVICE);
+
+        int simState = telephony_manager.getSimState(0); //slotIndex = 0 or 1
+        String result = "";
+        switch (simState) {
+            case TelephonyManager.SIM_STATE_ABSENT:
+                result = "No SIM card is available";
+
+            case TelephonyManager.SIM_STATE_NETWORK_LOCKED:
+                result = "Requires a network PIN to unlock";
+
+            case TelephonyManager.SIM_STATE_PIN_REQUIRED:
+                result = "Requires the user's SIM PIN to unlock";
+
+            case TelephonyManager.SIM_STATE_PUK_REQUIRED:
+                result = "Requires the user's SIM PUK to unlock";
+
+            case TelephonyManager.SIM_STATE_READY:
+                result =  "Sim slot is ready";
+
+            case TelephonyManager.SIM_STATE_UNKNOWN:
+                result = "Unknown Sim state";
+            default:
+                result = "Unknown Sim";
+        }
+        return result;
+    }
+
+    public static String getMACAddress(Context context){
+        WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wInfo = wm.getConnectionInfo();
+        @SuppressLint("MissingPermission") String macAddress = wInfo.getMacAddress();
+        return macAddress;
+    }
+
+//    public static int getPID (Intent intent) {
+//        int result = 1;
+//        if (intent != null) {
+//           if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
+//
+//                UsbDevice usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+//           if (usbDevice != null) {
+//                    int pid = usbDevice.getProductId();
+//                    result = pid;
+//                    return result;
+//                }
+//                else
+//                    result = 2;
+//
+//            }
+//            else
+//                result = 3;
+//        }
+//        return result;
+//    }
+
+    public static void getPIDVID (Context context, TextView textView1, TextView textView2,
+                                  TextView textView3) {
+        UsbManager usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+        HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
+        Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
+        StringBuilder stringbuilder1 = new StringBuilder();
+        StringBuilder stringbuilder2 = new StringBuilder();
+
+        StringBuilder stringBuilder1 = new StringBuilder();
+        StringBuilder stringBuilder2 = new StringBuilder();
+
+        if (deviceIterator != null) {
+            int Length = deviceList.size();
+            textView1.setText(Integer.toString(Length));
+            String Tag = "VendorID";
+
+            while (deviceIterator.hasNext()) {
+                UsbDevice usbDevice = deviceIterator.next();
+                int pid1 = usbDevice.getProductId();
+                String pid2 = Integer.toHexString(usbDevice.getProductId());
+                stringbuilder1.append(pid1 + ", ");
+                stringbuilder2.append(pid2 + ", ");
+                //textView2.setText(stringbuilder1.toString());
+                //textView3.setText(stringbuilder2);
+
+                int vid1 = usbDevice.getDeviceId();
+                String vid2 = Integer.toHexString(usbDevice.getDeviceId());
+                Log.i(Tag = "VendorID", String.valueOf(vid1));
+                Log.i(Tag = "VendorID hexa", String.valueOf(vid2));
+
+                Log.i(Tag = "ProductID", String.valueOf(pid1));
+                Log.i(Tag = "ProductID hexa", String.valueOf(pid2));  
+            }
+        }
+    }
 }
+
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,30 +1,37 @@
 package com.example.myapplication;
-import com.example.myapplication.Connectivity;
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.hardware.usb.*;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.telephony.SubscriptionManager;
 import android.view.View;
 import android.widget.Button;
-import android.hardware.usb.UsbDevice;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private String PERMISSIONS[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView PID = findViewById(R.id.product_id_txt);
-        TextView VID = findViewById(R.id.vendor_id_txt);
+
+        PERMISSIONS = new String[] {
+
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE
+        };
 
         Button Camera = (Button) findViewById(R.id.check_camera_btn);
         Button Sim = (Button) findViewById(R.id.check_sim_btn);
@@ -34,21 +41,27 @@ public class MainActivity extends AppCompatActivity {
         TextView WifiSSID = (TextView) findViewById(R.id.wifi_ssid_txt);
         TextView Connectiontype_des = (TextView) findViewById(R.id.textView7);
         TextView IPaddress = (TextView) findViewById(R.id.ip_address_txt);
+        TextView MACAddress = (TextView) findViewById(R.id.mac_address_txt);
+        TextView Number = findViewById(R.id.sim_number_txt);
+        TextView SimSlot1 = findViewById(R.id.sim_slot_number1_txt);
+        TextView SimSlot2 = findViewById(R.id.sim_slot_number2_txt);
+        TextView CameraNumber = findViewById(R.id.camera_numbers_txt);
+        TextView CameraError = findViewById(R.id.camera_errors_checking_txt);
+
+        TextView PID = findViewById(R.id.product_id_txt);
+        TextView VID = findViewById(R.id.vendor_id_txt);
+        TextView PNum = findViewById(R.id.product_name_txt);
+        TextView DID = findViewById(R.id.device_id_txt);
+        TextView PID_hex = findViewById(R.id.product_id_hexa_txt);
+
         Context context = getBaseContext();
+        Intent intent = new Intent(this, MainActivity.class);
+        SubscriptionManager subscriptionManager = (SubscriptionManager)
+                getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
 
+        if (!hasPermissions(MainActivity.this,PERMISSIONS)) {
 
-        Intent intent = getIntent();
-        if (intent != null) {
-            if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
-                UsbDevice usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                if (usbDevice != null) {
-
-                    int vid = usbDevice.getVendorId();
-                    int pid = usbDevice.getProductId();
-                    VID.setText(Integer.toString(vid));
-                    PID.setText(Integer.toString(pid));
-                }
-            }
+            ActivityCompat.requestPermissions(MainActivity.this,PERMISSIONS,1);
         }
 
 
@@ -56,35 +69,54 @@ public class MainActivity extends AppCompatActivity {
         Camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent camera = new Intent(MainActivity.this, Check_Camera.class);
-                startActivity(camera);
+                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                int cam_number;
+                try {
+                    if (cameraManager != null) {
+                        cam_number = cameraManager.getCameraIdList().length;
+                        CameraNumber.setText(Integer.toString(cam_number));
+                    }
+                }
+                catch (CameraAccessException e) {
+                    CameraError.setText("Unable to enumerate cameras");
+                }
+
+                Connectivity.getPIDVID(context,PNum,DID,PID_hex);
+
+
             }
         });
-
 
         Sim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent sim = new Intent(MainActivity.this, Check_Sim.class);
-                startActivity(sim);
+                int Sim_number = subscriptionManager.getActiveSubscriptionInfoCountMax();
+                Number.setText(Integer.toString(Sim_number));
+
+                String mainSimState = Connectivity.getSimState(context);
+                SimSlot1.setText(mainSimState);
+                String secondSimState = Connectivity.getSimState(context);
+                SimSlot2.setText(secondSimState);
             }
         });
-
-
-
 
         Wifi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Check connection of WIFI and Mobile data
                 if (Connectivity.isConnected(context)==true) {
                     if (Connectivity.isConnectedWifi(context) == true) {
+                        String mac = Connectivity.getMACAddress(context);
                         String ip = Connectivity.getDeviceIPAddress(context);
                         IPaddress.setText(ip);
                         Connectiontype.setText("Wifi connection");
+                        WifiSSID.setText(Connectivity.getWifiSSID(context));
+                        MACAddress.setText(mac);
                     }
 
                     //Mobile data connection
-                    else if (Connectivity.isConnectedWifi(context) == false && Connectivity.isConnectedMobile(context) == true){
+                    else if (Connectivity.isConnectedWifi(context) == false
+                            && Connectivity.isConnectedMobile(context) == true){
                         String mobile_data = Connectivity.getMobileNetworkType(context);
                         Connectiontype.setText(mobile_data);
                         Connectiontype_des.setText("Mobile data info:");
@@ -92,17 +124,47 @@ public class MainActivity extends AppCompatActivity {
                         WifiSSID.setText(networkinfo);
                         String ip = Connectivity.getDeviceIPAddress(context);
                         IPaddress.setText(ip);
-
                     }
                 }
-                else if (Connectivity.isConnected(context)==false)
+                else
                     Connectiontype.setText("No connection");
-
-
             }
         });
-
-
-
     }
+
+    private boolean hasPermissions(Context context, String... PERMISSIONS) {
+        if (context != null && PERMISSIONS != null) {
+            for (String permission: PERMISSIONS) {
+                if (ActivityCompat.checkSelfPermission(context,permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true; //return true if the device has permissions granted
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Access fine location is granted", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, "Access fine location is denied", Toast.LENGTH_SHORT).show();
+
+
+            if (grantResults[1] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Access coarse location is granted", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, "Access coarse location is denied", Toast.LENGTH_SHORT).show();
+
+
+            if (grantResults[2] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Read phone state is granted", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, "read phone state is denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
